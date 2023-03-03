b1eda1edf3c8243548243b5653289717af2d3f7a
Sergio Junior <sergiopromail@gmail.com>
2023-03-02 22:27:36 -0300

    implementation of the Create method on productController

--- Mango.Web/Controllers/ProductController.cs
+++ Mango.Web/Controllers/ProductController.cs
@@ -27,5 +27,27 @@ namespace Mango.Web.Controllers
 
             return View(list);
         }
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ProductDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productService.CreateProductAsync<ResponseDto>(model);
+
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+
+            return View(model);
+        }
     }
 }
